2eba643ce07489852bad875ca8e0b9fc6e858342
eXConf <sergey.sosedkin@gmail.com>
2021-04-04 11:35:27 +0300

    upd to v3

--- main.js
+++ main.js
@@ -7,30 +7,39 @@ robot.setKeyboardDelay(30)
 new Menu()
 Menu.setApplicationMenu(null)
 
+const RESOURCES_PATH = app.isPackaged
+  ? path.join(process.resourcesPath, 'assets')
+  : path.join(__dirname, '../assets')
+
+const getAssetPath = (...paths: string[]): string => {
+  return path.join(RESOURCES_PATH, ...paths)
+}
+
 let win
 
 function createWindow() {
-   win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 620,
     height: 800,
     minWidth: 400,
     minHeight: 450,
+    icon: getAssetPath('icon.png'),
     backgroundColor: 'white',
     webPreferences: {
-      nodeIntegration: false,
+      nodeIntegration: true,
       worldSafeExecuteJavaScript: true,
-      contextIsolation: true,
+      // contextIsolation: true,
       devTools: false,
-      preload: path.join(__dirname, 'preload.js')
-    }
+      // preload: path.join(__dirname, 'preload.js')
+    },
   })
 
   win.loadFile('./public/index.html')
-  win.setTitle('SIGMA X v2.1.2')
+  // win.setTitle('SIGMA X v2.1.2')
 
-  win.webContents.on('new-window', function(e, url) {
-    e.preventDefault();
-    require('electron').shell.openExternal(url);
+  win.webContents.on('new-window', function (e, url) {
+    e.preventDefault()
+    require('electron').shell.openExternal(url)
   })
 }
 
@@ -42,23 +51,21 @@ app.whenReady().then(createWindow)
 
 ipcMain.on('to-chat', (e, message) => {
   clipboard.writeText(message)
-  robot.keyToggle("alt", "down")
-  robot.keyTap("tab")
-  robot.keyToggle("alt", "up")
-  robot.keyToggle("shift", "down")
-  robot.keyTap("insert")
-  robot.keyToggle("shift", "up")
+  // Without delay the clipboard is often empty
   setTimeout(() => {
-    robot.keyTap("enter")
-    robot.keyToggle("alt", "down")
-    robot.keyTap("tab")
-    robot.keyToggle("alt", "up")
-  }, 90);
-  
-})
+    robot.keyToggle('alt', 'down')
+    robot.keyTap('tab')
+    robot.keyToggle('alt', 'up')
+    robot.keyToggle('shift', 'down')
+    robot.keyTap('insert')
+    robot.keyToggle('shift', 'up')
 
-// ipcMain.on('resize', (e, width) => {
-//   const height = win.getSize()[1]
-//   console.log(width, win.getSize()[0])
-//   // win.setSize(width + 10, height)
-// })
+    robot.keyTap('enter')
+    robot.keyToggle('alt', 'down')
+    robot.keyTap('tab')
+    robot.keyToggle('alt', 'up')
+  }, 90)
+})
+ipcMain.on('copy-to-buffer', (e, text) => {
+  clipboard.writeText(text)
+})
